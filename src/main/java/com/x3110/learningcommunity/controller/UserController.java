8051649d4527d2e0f5627c9d037a8864565418af
CastJo <34601111+CastJo@users.noreply.github.com>
2019-08-24 11:50:00 +0800

    跨域

--- src/main/java/com/x3110/learningcommunity/controller/UserController.java
+++ src/main/java/com/x3110/learningcommunity/controller/UserController.java
@@ -3,17 +3,14 @@ package com.x3110.learningcommunity.controller;
 import com.x3110.learningcommunity.model.User;
 import com.x3110.learningcommunity.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
     @Autowired
     UserService userService;
-
+    @CrossOrigin
     @RequestMapping(value="addPost", method = RequestMethod.POST)
     public int addPost(@RequestBody User user) {
         return userService.addUser(user);
