f55b7ee97b3980769375fd8d73569d66659e0fd2
whosl <982267311@qq.com>
2019-08-24 11:32:27 +0800

    新增adduser

--- src/main/java/com/x3110/learningcommunity/controller/UserController.java
+++ src/main/java/com/x3110/learningcommunity/controller/UserController.java
@@ -1,7 +1,11 @@
 package com.x3110.learningcommunity.controller;
 
+import com.x3110.learningcommunity.model.User;
 import com.x3110.learningcommunity.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +13,8 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-
+    @RequestMapping(value="addPost", method = RequestMethod.POST)
+    public int addPost(@RequestBody User user) {
+        return userService.addUser(user);
+    }
 }
--- src/main/java/com/x3110/learningcommunity/model/User.java
+++ src/main/java/com/x3110/learningcommunity/model/User.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 @Document(collection = "user")
 public class User {
     @Id
-    private String id;
+    private String userId;
 
     private String username;
 
@@ -18,12 +18,12 @@ public class User {
     @CreatedDate
     private LocalDateTime createdDate;
 
-    public String getId() {
-        return id;
+    public String getUserId() {
+        return userId;
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public void setUserId(String userId) {
+        this.userId = userId;
     }
 
     public String getUsername() {
