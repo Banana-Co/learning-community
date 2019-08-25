d61d109550919a6668397cf9a9f78cfb93e51eb4
lyx <383625226@qq.com>
2019-08-25 09:38:37 +0800

    Cross Origin

--- src/main/java/com/x3110/learningcommunity/controller/PostController.java
+++ src/main/java/com/x3110/learningcommunity/controller/PostController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/post")
+@CrossOrigin
 public class PostController {
     @Autowired
     PostService postService;
