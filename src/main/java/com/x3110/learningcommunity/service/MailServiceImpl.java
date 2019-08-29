154e24e854b5da706a96e70b1ad9f73365b40b6a
whosl <982267311@qq.com>
2019-08-29 21:34:16 +0800

    修改验证邮件的信息内容

--- src/main/java/com/x3110/learningcommunity/service/MailServiceImpl.java
+++ src/main/java/com/x3110/learningcommunity/service/MailServiceImpl.java
@@ -7,8 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import java.util.Random;
 
 @Service
@@ -17,27 +20,44 @@ public class MailServiceImpl implements MailService {
     @Autowired
     private JavaMailSender mailSender;
 
-    public static final String SINGLE_EMAIL_REGEX = "(?:(?:[A-Za-z0-9\\-_@!#$%&'*+/=? ^`{|}~]|(?:\\\\[\\x00-\\xFF]?)|(?:\"[\\x00-\\xFF]*\"))+(?:\\.(?:(?:[A-Za-z0-9\\-_@!#$%&'*+/=?^`{|}~])|(?:\\\\[\\x00-\\xFF]?)|(?:\"[\\x00-\\xFF]*\"))+)*)@(?:(?:[A-Za-z0-9](?:[A-Za-z0-9-]*[A-Za-z0-9])?\\.)+(?:(?:[A-Za-z0-9]*[A-Za-z][A-Za-z0-9]*)(?:[A-Za-z0-9-]*[A-Za-z0-9])?))";
+    public static final String SINGLE_EMAIL_REGEX = "(?:(?:[A-Za-z0-9\\-_@!#$%&'*+/=?^`{|}~]|(?:\\\\[\\x00-\\xFF]?)|(?:\"[\\x00-\\xFF]*\"))+(?:\\.(?:(?:[A-Za-z0-9\\-_@!#$%&'*+/=?^`{|}~])|(?:\\\\[\\x00-\\xFF]?)|(?:\"[\\x00-\\xFF]*\"))+)*)@(?:(?:[A-Za-z0-9](?:[A-Za-z0-9-]*[A-Za-z0-9])?\\.)+(?:(?:[A-Za-z0-9]*[A-Za-z][A-Za-z0-9]*)(?:[A-Za-z0-9-]*[A-Za-z0-9])?))";
+
 
     @Override
     public Result sendPin(String toEmail) {
         if(!validEmailAddress(toEmail))return ResultFactory.buildFailResult(ResultCode.INVALID_EMAIL_ADDRESS);
-        SimpleMailMessage message=new SimpleMailMessage();
-        message.setFrom("learningCom110@163.com");
-        message.setTo(toEmail);
-        message.setSubject("学习生活交流论坛登录验证");
-        String str="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
         String uuid=new String();
-        for(int i=0;i<6;i++) {
-            char ch = str.charAt(new Random().nextInt(str.length()));
-            uuid += ch;
+        try{
+            MimeMessage msg = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(msg,true,"utf-8");
+            helper.setFrom("learningCom110@163.com");//发件人
+            helper.setTo(toEmail);//收件人
+            helper.setSubject("学习生活交流论坛登录验证");//邮件标题
+            //验证码
+            String str="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            for(int i=0;i<6;i++) {
+                char ch = str.charAt(new Random().nextInt(str.length()));
+                uuid += ch;
+            }
+            String message = "<div><font face=\"黑体\"><font size=\"4\" style=\"\">您的注册验证码是：</font><b style=\"\"><u style=\"\"><font size=\"5\">" +
+                    uuid+"</font></u></b><font size=\"4\" style=\"\">（区分大小写），欢迎使用学习生活交流论坛。</font></font></div>" +
+                    "<div><font face=\"黑体\"><font size=\"4\" style=\"\">这个验证码将在您收到这封邮件5分钟后失效</font><font size=\"4\" style=\"\">。</font></font></div>";
+            helper.setText(message, true);
+            mailSender.send(msg);
+        }catch (MessagingException e){
+            return ResultFactory.buildFailResult(e.getMessage());
         }
-        message.setText("您的注册验证码是："+uuid+"（区分大小写），"+"欢迎使用学习生活交流论坛");
-        mailSender.send(message);
+//        SimpleMailMessage message=new SimpleMailMessage();
+//        message.setFrom("learningCom110@163.com");
+//        message.setTo(toEmail);
+//        message.setSubject("学习生活交流论坛登录验证");
         return ResultFactory.buildSuccessResult(uuid);
     }
 
     Boolean validEmailAddress(String emailAddress){
         return emailAddress.matches(SINGLE_EMAIL_REGEX);
     }
+
+
+
 }
